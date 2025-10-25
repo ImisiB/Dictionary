38e33ca6c233860c18ec4870eadb6faff40c89c7
Imisi Bailey <imisibailey@gmail.com>
2025-10-25 13:02:55 +0100

    Created a dictionary

--- dictionary.js
+++ dictionary.js
@@ -27,7 +27,57 @@ document.querySelector('.js-button')
     const getAPI = async() => {
       const response = await fetch(link)
       const data = await response.json()
-      console.log(data)
+      const definition = data.entries[0].senses[0].definition
+      const definition2 = data.entries[0].senses[1].definition
+      const definition3 = data.entries[0].senses[2].definition
+      const example = data.entries[0].senses[0].examples
+      const example1 = data.entries[0].senses[1].examples
+      const example2 = data.entries[0].senses[2].examples
+      document.querySelector('.definition').innerHTML = `Definition 1: ${definition}`
+      document.querySelector('.definition2').innerHTML = `Definition 2: ${definition2}`
+      document.querySelector('.definition3').innerHTML = `Definition 3: ${definition3}`
+      if(example == '') {
+        document.querySelector('.example').innerHTML = ``
+      } else if(example1 == '') {
+        document.querySelector('.example2').innerHTML = ``
+      } else if(example2 == '') {
+        document.querySelector('.example3').innerHTML = ``
+      } else {
+        document.querySelector('.example').innerHTML = `Example 1: ${example}`
+        document.querySelector('.example2').innerHTML = `Example 2: ${example1}`
+        document.querySelector('.example3').innerHTML = `Example 3: ${example2}`
+      }
     }
     getAPI();
 })
+
+document.body.onkeydown = (event) => {
+  if (event.key === "Enter") {
+    const link = `https://freedictionaryapi.com/api/v1/entries/en/${userWord.value}`
+    const getAPI = async() => {
+      const response = await fetch(link)
+      const data = await response.json()
+      const definition = data.entries[0].senses[0].definition
+      const definition2 = data.entries[0].senses[1].definition
+      const definition3 = data.entries[0].senses[2].definition
+      const example = data.entries[0].senses[0].examples
+      const example1 = data.entries[0].senses[1].examples
+      const example2 = data.entries[0].senses[2].examples
+      document.querySelector('.definition').innerHTML = `Definition 1: ${definition}`
+      document.querySelector('.definition2').innerHTML = `Definition 2: ${definition2}`
+      document.querySelector('.definition3').innerHTML = `Definition 3: ${definition3}`
+      if(example == '') {
+        document.querySelector('.example').innerHTML = ``
+      } else if(example1 == '') {
+        document.querySelector('.example2').innerHTML = ``
+      } else if(example2 == '') {
+        document.querySelector('.example3').innerHTML = ``
+      } else {
+        document.querySelector('.example').innerHTML = `Example 1: ${example}`
+        document.querySelector('.example2').innerHTML = `Example 2: ${example1}`
+        document.querySelector('.example3').innerHTML = `Example 3: ${example2}`
+      }
+    }
+    getAPI()
+  }
+};
